eca10e548cddf51636354510d7638599026b0132
sage-lyon <sage_lyon@student.uml.edu>
2022-03-04 12:17:39 -0500

    Initial commit of main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cstring>
+#include "CL/opencl.h"
+#include "AOCLUtils/aocl_utils.h"
+
+using namespace aocl_utils;
+
+#define STRING_BUFFER_LEN 1024
+
+// OpenCL runtime configuration
+static cl_platform_id platform = NULL;
+static cl_device_id device = NULL;
+static cl_context context = NULL;
+static cl_command_queue queue = NULL;
+static cl_kernel kernel = NULL;
+static cl_program program = NULL;
+
+void cleanup();
+
+int main() {
+
+}
