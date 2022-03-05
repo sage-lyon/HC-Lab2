f7a4fec43071a4b075883b5de6ad60d196572fff
sage-lyon <sage_lyon@student.uml.edu>
2022-03-04 20:57:08 -0500

    Added functions to create platform, device, context, command queue, and kernel

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <CL/cl.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,5 +22,44 @@ static cl_program program = NULL;
 void cleanup();
 
 int main() {
+    cl_int status;
+
+    // Find FPGA Emulator platform
+    platform = findPlatform("Intel(R) FPGA Emulation Platform for OpenCL(TM)");
+
+    if(platform == NULL) {
+        printf("ERROR: Cannot find platform\n");
+        return -1;
+    }
+
+    // Find available OpenCL device
+    cl_uint num_devices;
+    getDevices(platform, CL_DEVICE_TYPE_ALL, &num_devices);
+    status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device, &num_devices);
+    checkError(status, "Unable to get device ID");
+
+    // Create context
+    context = clCreateContext(NULL, 1, &device, NULL, NULL, &status);
+    checkError(status, "Unable to create context");
+
+    // Create command queue
+    queue = clCreateCommandQueue(context, device, 0, &status);
+    checkError(status, "Unable to create command queue");
+
+    // Create program
+    std::string binary_file = getBoardBinaryFile("picalc", device);
+    program = createProgramFromBinary(context, binary_file.c_str(), &device, 1);
+
+    // Build program
+    status = clBuildProgram(program, 0, NULL, "", NULL, NULL);
+    checkError(status, "Unable to build program");
+
+    // Create kernel
+    const char* kernel_name = "picalc";
+    kernel = clCreateKernel(program, kernel_name, &status);
+    checkError(status, "Unable to create kernel");
+
+    // Enqueue and launch kernel
+    status = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, gSize, lSize, 0, NULL, NULL);
 
 }
