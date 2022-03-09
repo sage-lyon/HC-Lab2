ca669b90110be646967a7d7be720c50f53a0ac05
sage-lyon <sage_lyon@student.uml.edu>
2022-03-08 22:22:32 -0500

    Added functions to set kernel arguments, execute kernel, and read result from kernel

--- main.cpp
+++ main.cpp
@@ -11,18 +11,24 @@ using namespace aocl_utils;
 
 #define STRING_BUFFER_LEN 1024
 
-// OpenCL runtime configuration
-static cl_platform_id platform = NULL;
-static cl_device_id device = NULL;
-static cl_context context = NULL;
-static cl_command_queue queue = NULL;
-static cl_kernel kernel = NULL;
-static cl_program program = NULL;
+#define LOCAL_SIZE 0
+#define WORKGROUPS 0
 
 void cleanup();
 
 int main() {
+
+    // OpenCL runtime configuration
+    cl_platform_id platform = NULL;
+    cl_device_id device = NULL;
+    cl_context context = NULL;
+    cl_command_queue queue = NULL;
+    cl_kernel kernel = NULL;
+    cl_program program = NULL;
+
     cl_int status;
+
+    double result;
 
     // Find FPGA Emulator platform
     platform = findPlatform("Intel(R) FPGA Emulation Platform for OpenCL(TM)");
@@ -59,7 +65,28 @@ int main() {
     kernel = clCreateKernel(program, kernel_name, &status);
     checkError(status, "Unable to create kernel");
 
+    // Set kernel arguments 
+    int operands_per_item = 16;
+    cl_mem result_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(result), result, NULL);
+    
+    status = clSetKernelArg(kernel, 0, sizeof(operands_per_item), &operands_per_item);
+    status |= clSetKernelArg(kernel, 1, LOCAL_SIZE * sizeof(double), NULL);
+    status |= clSetKernelArg(kernel, 2, WORKGROUPS * sizeof(double), NULL);
+    status |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &result_buffer);
+    checkError(status, "Unable to set kernel arguments");
+
     // Enqueue and launch kernel
+    size_t gSize[3] = {WORKGROUPS * LOCAL_SIZE, 1 , 1};
+    size_t lSize[3] = {LOCAL_SIZE, 1 , 1};
     status = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, gSize, lSize, 0, NULL, NULL);
+    checkError(status, "Unable to launch kernel");
+
+    // Get results from kernel
+    ret = clEnqueueReadBuffer(queue, result_buffer, CL_TRUE, 0, sizeof(result), &result, 0, NULL, NULL);
+
+    // Print result
+    printf("Result: %lf\n", result);
+
+    return 0;
 
 }
